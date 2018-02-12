fbf2e14ff61801a4f7a1492d0d609f801d50fb35
ryo nakamaru <pottava@gmail.com>
2018-02-12 15:40:26 +0900

    change user dir permission & chrome binary path (v0.11)

--- versions/0.11jp/index.js
+++ versions/0.11jp/index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async() => {
 
     const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/chromium-browser'
+        executablePath: 'chromium-browser'
     });
     const page = await browser.newPage();
     await page.goto('https://example.com');
